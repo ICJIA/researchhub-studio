e274472b80f2ace1ff4321ad9c51c6098977c913
Bobae Kang <bkang.dev@gmail.com>
2019-08-02 13:39:59 -0500

    refactor: load katex dynamically

--- src/components/MarkdownEditor.vue
+++ src/components/MarkdownEditor.vue
@@ -35,16 +35,22 @@
 </template>
 
 <script>
-const mdOpts = {
+import MarkdownItTexmath from 'markdown-it-texmath'
+
+const md = require('markdown-it')({
   html: true,
   linkify: true,
   typographer: true
-}
+}).use(require('markdown-it-footnote'))
 
-const md = require('markdown-it')(mdOpts)
-  .use(require('markdown-it-footnote'))
-  // eslint-disable-next-line no-undef
-  .use(require('markdown-it-texmath').use(katex))
+const loadFromCDN = (tagName, attrs) => {
+  const el = document.createElement(tagName)
+  Object.keys(attrs).forEach(key => el.setAttribute(key, attrs[key]))
+  document.head.appendChild(el)
+
+  // eslint-disable-next-line no-unused-vars
+  return new Promise((resolve, reject) => el.addEventListener('load', resolve))
+}
 
 function syncScroll(from, to) {
   let sf = from.scrollHeight - from.clientHeight
@@ -76,6 +82,29 @@ export default {
     preview() {
       return this.$refs.myPreview
     }
+  },
+  async created() {
+    if (!document.head.querySelector('#katexCSS'))
+      await loadFromCDN('link', {
+        id: 'katexCSS',
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/katex@0.10.2/dist/katex.min.css',
+        integrity:
+          'sha384-yFRtMMDnQtDRO8rLpMIKrtPCD5jdktao2TV19YiZYWMDkUR5GQZR/NOVTdquEx1j',
+        crossorigin: 'anonymous'
+      })
+
+    if (!document.head.querySelector('#katexJS'))
+      await loadFromCDN('script', {
+        id: 'katexJS',
+        src: 'https://cdn.jsdelivr.net/npm/katex@0.10.2/dist/katex.min.js',
+        integrity:
+          'sha384-9Nhn55MVVN0/4OFx7EE5kpFBPsEMZxKTCnA+4fqDmg12eCTqGi6+BB2LjY8brQxJ',
+        crossorigin: 'anonymous'
+      })
+
+    // eslint-disable-next-line no-undef
+    md.use(MarkdownItTexmath.use(katex))
   },
   mounted() {
     this.markdownLocal = this.markdown
