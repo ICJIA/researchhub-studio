6d710c56b64f032cddc989b764fac9c298a65753
Bobae Kang <bkang.kr@gmail.com>
2019-08-08 16:34:31 -0500

    fix: add missing view title to error pages

--- src/components/ServerError.vue
+++ src/components/ServerError.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="error-page">
-    <v-icon x-large color="error">cancel</v-icon>
-    <h1 class="pb-3">Sorry, something went wrong</h1>
+  <div>
+    <BaseViewTitle page="Error" />
 
-    <p>Try refreshing the page or please check back later.</p>
+    <div class="error-page">
+      <v-icon x-large color="error">cancel</v-icon>
+      <h1 class="pb-3">Sorry, something went wrong</h1>
+
+      <p>Try refreshing the page or please check back later.</p>
+    </div>
   </div>
 </template>
+
+<script>
+const BaseViewTitle = () => import('@/components/BaseViewTitle')
+
+export default {
+  components: {
+    BaseViewTitle
+  }
+}
+</script>
--- src/views/404.vue
+++ src/views/404.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="error-page">
-    <v-icon x-large color="error">cancel</v-icon>
-    <h1 class="pb-3">Page not found</h1>
+  <div>
+    <BaseViewTitle page="Error" />
 
-    <p>Sorry. The page you are looking for does not exist.</p>
-    <v-btn flat to="/">return to home <v-icon>replay</v-icon></v-btn>
+    <div class="error-page">
+      <v-icon x-large color="error">cancel</v-icon>
+      <h1 class="pb-3">Page not found</h1>
+
+      <p>Sorry. The page you are looking for does not exist.</p>
+      <v-btn flat to="/">return to home <v-icon>replay</v-icon></v-btn>
+    </div>
   </div>
 </template>
+
+<script>
+const BaseViewTitle = () => import('@/components/BaseViewTitle')
+
+export default {
+  components: {
+    BaseViewTitle
+  }
+}
+</script>
