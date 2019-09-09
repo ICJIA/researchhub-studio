8904e08e5d51dd665ea4f75c107642ec4bf72199
Bobae Kang <bkang.dev@gmail.com>
2019-09-09 09:26:25 -0500

    feat: a help button to the author guide

---
+++ src/components/AuthorGuideButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-btn text :href="href" target="_blank">
+    <v-icon class="small" color="warning">mdi-help-circle</v-icon>
+    <span class="px-1 small">{{ text }}</span>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    path: {
+      type: String,
+      default: ''
+    },
+    text: {
+      type: String,
+      default: 'Go to Author Guide'
+    }
+  },
+  computed: {
+    href() {
+      return `${process.env.VUE_APP_MAIN_BASE_URL}/docs/auth-guide/${this.path}`
+    }
+  }
+}
+</script>
--- src/components/BaseViewLayout.vue
+++ src/components/BaseViewLayout.vue
@@ -4,19 +4,31 @@
 
     <v-col class="mx-auto font-lato" cols="12" sm="10" lg="8" xl="7">
       <template v-if="page">
-        <h2 class="bold"><slot name="title"></slot></h2>
+        <v-row justify="space-between" no-gutters>
+          <h2 class="bold"><slot name="title"></slot></h2>
+
+          <AuthorGuideButton
+            v-if="isAuthor"
+            :path="`tutorials/${page.toLowerCase()}-article`"
+            text="Go to tutorial page"
+          />
+        </v-row>
+
         <p class="py-2"><slot name="description"></slot></p>
       </template>
+
       <slot></slot>
     </v-col>
   </div>
 </template>
 
 <script>
+const AuthorGuideButton = () => import('@/components/AuthorGuideButton')
 const BaseViewTitle = () => import('@/components/BaseViewTitle')
 
 export default {
   components: {
+    AuthorGuideButton,
     BaseViewTitle
   },
   computed: {
@@ -28,6 +40,9 @@ export default {
             .split(' ')
             .map(el => `${el.charAt(0).toUpperCase()}${el.slice(1)}`)
             .join(' ')
+    },
+    isAuthor() {
+      return this.$store.state.auth.role === 'Author'
     }
   }
 }
