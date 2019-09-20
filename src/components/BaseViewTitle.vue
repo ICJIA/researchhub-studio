910e826fd2724ad114a460d81c26ab258a513c77
Bobae Kang <bkang.dev@gmail.com>
2019-09-20 18:08:47 -0500

    refactor: use CSS to capitalize words

--- src/components/BaseViewTitle.vue
+++ src/components/BaseViewTitle.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="view-title font-lato">
     <v-col class="mx-auto py-2" cols="12" sm="10" lg="8" xl="7">
-      <h4 class="font-weight-light">
+      <h4 class="font-weight-light text-capitalize">
         <template>{{ 'ICJIA Research Hub' }}</template>
         <v-icon color="white">{{ $options.static.mdiChevronRight }}</v-icon>
-        <template>{{ 'Studio' }}</template>
+        <template>{{ 'studio' }}</template>
         <template v-if="page">
           <v-icon color="white">{{ $options.static.mdiChevronRight }}</v-icon>
           <template>{{ page }}</template>
