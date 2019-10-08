e2598d9f93311bf38e20657e12737d96114ac0ea
Bobae Kang <bkang.dev@gmail.com>
2019-10-08 11:18:37 -0500

    refactor: use BASE_URL direclty if possible

--- src/router.js
+++ src/router.js
@@ -6,10 +6,7 @@ import store from '@/store/index'
 Vue.use(Router)
 
 const router = new Router({
-  base:
-    process.env.NODE_ENV === `production`
-      ? process.env.VUE_APP_PUBLIC_PATH
-      : '/',
+  base: process.env.BASE_URL,
   mode: 'history',
   routes: [
     {
